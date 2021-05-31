made the base model for the frame attention network more customizable fixed error in the first attention calculation fixed error in the default architecture of the emotionnet nano model

models/frame_attention.py
@@ -61,11 +61,12 @@ def resnet(blocks, image_shape):
     return Model(inputs=inputs, outputs=out, name='resnet')
 
 
-def fanet(res_blocks=(2, 2, 2, 2), image_shape=(48, 48, 1), frames=5,
-          attention_type='self', base_builder=resnet, num_classes=6) -> Model:
+def fanet(image_shape=(48, 48, 1), frames=5,
+          attention_type='self', base=None, num_classes=6) -> Model:
     # input shape should be (examples, frames, w, h, d)
     inputs = layers.Input(shape=(frames,) + image_shape)
-    base = base_builder(res_blocks, image_shape)
+    if base is None:
+        base = resnet((2, 2, 2, 2), image_shape)
     alpha = layers.Dense(1, activation='sigmoid')
     beta = layers.Dense(1, activation='sigmoid')
     dropout = layers.Dropout(0.5)
@@ -85,7 +86,7 @@ def fanet(res_blocks=(2, 2, 2, 2), image_shape=(48, 48, 1), frames=5,
     alphas_stack = tf.stack(alphas, axis=1)
 
     # calculate self attention score
-    attention = tf.reduce_sum(outputs_stack * alphas_stack, axis=1) / tf.reduce_sum(outputs_stack, axis=1)
+    attention = tf.reduce_sum(outputs_stack * alphas_stack, axis=1) / tf.reduce_sum(alphas_stack, axis=1)
 
     if attention_type == 'relation':
         betas = []